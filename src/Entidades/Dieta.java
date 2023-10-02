a77c6dbe877ee043ff652b8d2272ece9ad714f8c
JNZader <javierzader@hotmail.com>
2023-10-02 20:40:31 -0300

    Entidades Dieta

--- src/Entidades/Dieta.java
+++ src/Entidades/Dieta.java
@@ -1,14 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Entidades;
 
-/**
- *
- * @author javie
- */
+import java.time.LocalDate;
+
 public class Dieta {
+    private int idDieta;
+    private String nombre;
+    private Paciente paciente;
+    private LocalDate fechaInicial;
+    private LocalDate fechaFinal;
+    private double pesoFinal;
+
+    public Dieta() {
+    }
+
+    public Dieta(String nombre, Paciente paciente, LocalDate fechaInicial, LocalDate fechaFinal, double pesoFinal) {
+        this.nombre = nombre;
+        this.paciente = paciente;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
+        this.pesoFinal = pesoFinal;
+    }
+
+    public Dieta(int idDieta, String nombre, Paciente paciente, LocalDate fechaInicial, LocalDate fechaFinal, double pesoFinal) {
+        this.idDieta = idDieta;
+        this.nombre = nombre;
+        this.paciente = paciente;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
+        this.pesoFinal = pesoFinal;
+    }
+
+    public int getIdDieta() {
+        return idDieta;
+    }
+
+    public void setIdDieta(int idDieta) {
+        this.idDieta = idDieta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public LocalDate getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(LocalDate fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public LocalDate getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(LocalDate fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public double getPesoFinal() {
+        return pesoFinal;
+    }
+
+    public void setPesoFinal(double pesoFinal) {
+        this.pesoFinal = pesoFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Dieta{" + "idDieta=" + idDieta + ", nombre=" + nombre + ", paciente=" + paciente + ", fechaInicial=" + fechaInicial + ", fechaFinal=" + fechaFinal + ", pesoFinal=" + pesoFinal + '}';
+    }
     
 }
